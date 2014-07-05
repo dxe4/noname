c73295447a9b76d64e696a7b1ec2606739762788
Charalampos Papaloizou <papaloizouc@gmail.com>
2014-07-05 12:55:25 +0100

    tags

--- processing.py
+++ processing.py
@@ -1,9 +1,32 @@
 from pprint import pprint
-from nltk import Text, word_tokenize
+from nltk import Text, word_tokenize, pos_tag
 import redis
 
 subreddit_info_keys = ['name', 'url']
 subreddit_category_keys = ['top_week', 'hot', 'top_day', 'top_year', 'top_month']
+
+"""
+ADJ	adjective	new, good, high, special, big, local
+ADV	adverb	really, already, still, early, now
+CNJ	conjunction	and, or, but, if, while, although
+DET	determiner	the, a, some, most, every, no
+EX	existential	there, there's
+FW	foreign word	dolce, ersatz, esprit, quo, maitre
+MOD	modal verb	will, can, would, may, must, should
+N	noun	year, home, costs, time, education
+NP	proper noun	Alison, Africa, April, Washington
+NUM	number	twenty-four, fourth, 1991, 14:24
+PRO	pronoun	he, their, her, its, my, I, us
+P	preposition	on, of, at, with, by, into, under
+TO	the word to	to
+UH	interjection	ah, bang, ha, whee, hmpf, oops
+V	verb	is, has, get, do, make, see, run
+VD	past tense	said, took, told, made, asked
+VG	present participle	making, going, playing, working
+VN	past participle	given, taken, begun, sung
+WH	wh determiner	who, which, when, what, where, how
+"""
+
 
 def get_stopwords():
     lines = []
@@ -41,6 +64,8 @@ def process_post(post):
 
     if text:
         tokens = word_tokenize(text)
+        tagged = pos_tag(tokens)
+        print(tagged)
         ntlk_text = Text(tokens)
 
     comment_score_sum = sum([i["score"] for i in comments])
