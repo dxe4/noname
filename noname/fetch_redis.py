994bab62767e26164279c943d3076060d7f99924
foobar <foobar@crunchbang.(none)>
2014-07-13 23:29:53 +0100

    implement recursion & raise error if failed

--- noname/fetch_redis.py
+++ noname/fetch_redis.py
@@ -1,17 +1,7 @@
+from copy import deepcopy
 import requests
-from itertools import chain
+# from itertools import chain
 # from collections import namedtuple
-
-
-class TopOptions(object):
-    '''
-    Just for autocomplete reasons, could be a dict
-    '''
-    def __init__(self, t='year', sort='top', limit=100, after=None):
-        self.t = t
-        self.sort = sort
-        self.limit = limit
-        self.after = after
 
 
 USER_AGENT = 'noname version:1 url:https://github.com/papaloizouc/noname'
@@ -19,10 +9,24 @@ DEAFAULT_HEADERS = {
     'User-Agent': USER_AGENT
 }
 
-# some crazy staff
-post_types = {'text', 'url', 'url_text'}
-# optional_input = {'url': 2, 'selftext': 1, 'domain': 0}
-# cast_post_type = {1: 'text', 2: 'url', 3: 'url_text'}
+
+class FetchError(Exception):
+    pass
+
+
+class TopOptions(object):
+
+    '''
+    Just for autocomplete reasons, could be a dict
+    '''
+
+    def __init__(self, t='year', sort='top', limit=100, after=None,
+                 before=None):
+        self.t = t
+        self.sort = sort
+        self.limit = limit
+        self.after = after
+        self.before = before
 
 
 def _determine_type(domain, text):
@@ -69,7 +73,7 @@ def parse_posts(children):
             for child in children]
 
 
-def get_subreddit_top_recursive(top_options, depth=1):
+def get_subreddit_top_recursive(subreddit, top_options=None, depth=1):
     '''
         :param top_options: Instance of TopOptions
         Just to avoid the recursion logic (if statements) in get_subreddit_top
@@ -79,29 +83,46 @@ def get_subreddit_top_recursive(top_options, depth=1):
     if depth <= 0:
         raise ValueError('Depth must be bigger than 0, given {}'.format(depth))
 
+    if top_options is None:
+        top_options = TopOptions()
+
     posts = []
     for i in range(0, depth):
-        #  TODO handle next arguement
-        result = get_subreddit_top(top_options)
+        result = get_subreddit_top(subreddit, top_options=top_options)
+        top_options = deepcopy(top_options)
+        top_options.after = result['after']
         posts.append(result)
 
-    return chain(*posts)
+    return posts
 
 
-def get_subreddit_top(top_options):
+def get_subreddit_top(subreddit, top_options=None):
     '''
         :param top_options: Instance of TopOptions
         Calls http://www.reddit.com/dev/api#GET_{sort}
     '''
-    response = requests.get(url='http://reddit.com/r/python/top.json',
+    if top_options is None:
+        top_options = TopOptions()
+
+    url = 'http://reddit.com/r/{}/top.json'.format(subreddit)
+    response = requests.get(url=url,
                             headers=DEAFAULT_HEADERS,
                             params=vars(top_options))
+
+    if response.status_code != 200:
+        raise FetchError("Couldn't fetch subreddit {}. status_code: {}".format(
+            subreddit, response.status_code))
 
     data = response.json()['data']
     children, after, before = data['children'], data['after'], data['before']
     posts = parse_posts(children)
-    #  TODO return dict instead of list
-    return posts
+
+    result = {
+        'posts': posts,
+        'after': after,
+        'before': before,
+    }
+    return result
 
 if __name__ == '__main__':
-    get_subreddit_top()
+    res = get_subreddit_top_recursive('python', depth=2)
